ce187a6a668da8b47d46467f868c6e3ae1e31aaa
Gerson coder <88016026+GersonAmade@users.noreply.github.com>
2024-07-01 17:56:51 -0300

    Update config.js

--- config.js
+++ config.js
@@ -3,15 +3,15 @@ const chalk = require('chalk');
 
 
 // Other
-global.link = 'https://whatsapp.com/channel/0029VaWJMi3GehEE9e1YsI1S'
-global.linkGroup = 'https://chat.whatsapp.com/E3PWxdvLc7ZCp1ExOCkEGp'
+global.link = 'https://whatsapp.com/channel/0029VaeAalC6BIEYqoqlr41c'
+global.linkGroup = 'https://chat.whatsapp.com/GWgALrT6peC3sf10a8xuHE'
 
 global.apikey = 'bf2d2cf29b3edc604b447983';
-global.ownername = "𝑆𝛪𝐷 𝐵𝛨𝛥𝛪";
-global.owner = ['917050906659'];
-global.premium = ['917050906659'];
+global.ownername = "Mozsound9 Bot";
+global.owner = ['258833868906'];
+global.premium = ['258850442534'];
 global.packname = "𝚜𝚝𝚒𝚌𝚔𝚎𝚛 𝚋𝚢";
-global.author = "𝐆𝐒𝐒 𝚩𝚯𝚻𝐖𝚫";
+global.author = "Gerson Amade";
 global.sessionName = 'session';
 global.prefa = [".", "!", "/", "?"];
 global.typemenu = "v1"
